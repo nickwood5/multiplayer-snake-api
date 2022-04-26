1909f92612f401a732ffdc8df32cd07a8b938a0b
Nicholas Wood <horizon.nicholas14@gmail.com>
2022-04-26 00:49:55 -0400

    Restructured API to operate independently from application head

--- app.py
+++ app.py
@@ -1,20 +1,37 @@
+from importlib.resources import Resource
 from flask import Flask, jsonify
 import flask, websockets
 from waitress import serve
-
-
-async def get_user_id():
-    async with websockets.connect("ws://127.0.0.1:8081") as websocket:
-        user_id = await websocket.recv()
-        return user_id
+from flask_restful import Api
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route('/')
-async def aaaa():
-    user_id = await get_user_id()
-    resp = flask.make_response(jsonify({"Nick API": "HELLO"}))
-    return resp
+connected_ids = []
+
+class addId(Resource):
+    def get(self, id):
+        if id not in connected_ids:
+            connected_ids.append(id)
+            resp = flask.make_response(jsonify({"API": "success"}))
+        else:
+            resp = flask.make_response(jsonify({"API": "id_already_exists"}))
+        
+        print("Connected ids is now {}".format(connected_ids))
+        return resp
+
+class removeId(Resource):
+    def get(self, id):
+        if id in connected_ids:
+            connected_ids.remove(id)
+            resp = flask.make_response(jsonify({"API": "success"}))
+        else:
+            resp = flask.make_response(jsonify({"API": "id_does_not_exist"}))
+        print("Connected ids is now {}".format(connected_ids))
+        return resp
+
+api.add_resource(addId, "/add/<int:id>")
+api.add_resource(removeId, "/remove/<int:id>")
 
 if __name__ == '__main__':
     serve(app, host='0.0.0.0', port=8080)
