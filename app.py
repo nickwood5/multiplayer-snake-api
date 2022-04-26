0a57ee3e8972362e864d7c95328cd96441f5eed5
Nicholas Wood <horizon.nicholas14@gmail.com>
2022-04-26 01:04:34 -0400

    Redo app routing functions

--- app.py
+++ app.py
@@ -9,29 +9,24 @@ api = Api(app)
 
 connected_ids = []
 
-class addId(Resource):
-    def get(self, id):
-        if id not in connected_ids:
-            connected_ids.append(id)
-            resp = flask.make_response(jsonify({"API": "success"}))
-        else:
-            resp = flask.make_response(jsonify({"API": "id_already_exists"}))
-        
-        print("Connected ids is now {}".format(connected_ids))
-        return resp
+@app.route('/remove/<int:id>/')
+def remove(id):
+    if id in connected_ids:
+        connected_ids.remove(id)
+        resp = flask.make_response(jsonify({"API": "success"}))
+    else:
+        resp = flask.make_response(jsonify({"API": "id_does_not_exist"}))
+    print("Connected ids is now {}".format(connected_ids))
+    return resp
 
-class removeId(Resource):
-    def get(self, id):
-        if id in connected_ids:
-            connected_ids.remove(id)
-            resp = flask.make_response(jsonify({"API": "success"}))
-        else:
-            resp = flask.make_response(jsonify({"API": "id_does_not_exist"}))
-        print("Connected ids is now {}".format(connected_ids))
-        return resp
-
-api.add_resource(addId, "/add/<int:id>")
-api.add_resource(removeId, "/remove/<int:id>")
+@app.route('/get/')
+def get(id):
+    id = 0
+    while id in connected_ids:
+        id += 1
+    connected_ids.append(id)
+    resp = flask.make_response(jsonify({"id": id}))
+    return resp
 
 if __name__ == '__main__':
     serve(app, host='0.0.0.0', port=8080)
